c4a6899432321fb5e2843721c3a8106f90637eda
Vadim Samoilenko <samoilenko26@gmail.com>
2023-07-21 20:16:07 +0200

    more tests for create_application method added

--- interview_tracker/tests/test_create_application.py
+++ interview_tracker/tests/test_create_application.py
@@ -1,3 +1,5 @@
+import json
+
 import pytest
 from fastapi import FastAPI, status
 from httpx import AsyncClient
@@ -73,3 +75,122 @@ async def test_create_application_okay(  # noqa: WPS218
     ):
         assert created_timeline.name == requested_timeline.name
         assert created_timeline.value == requested_timeline.value
+
+
+@pytest.mark.anyio
+async def test_create_application_company_name_not_valid(  # noqa:WPS118
+    client: AsyncClient,
+    dbsession: AsyncSession,
+    fastapi_app: FastAPI,
+    application_request_body: ApplicationCreateMessage,
+) -> None:
+
+    application_request_body.company_name = ""
+    request_body = application_request_body.dict()
+    url = fastapi_app.url_path_for("create_application")
+    headers = get_user_token_headers()
+    response = await client.post(url=url, headers=headers, json=request_body)
+    response_data = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response_data["detail"] == [
+        {
+            "loc": ["body", "company_name"],
+            "msg": "company_name cannot be less 1 characters",
+            "type": "value_error",
+        },
+    ]
+
+
+@pytest.mark.anyio
+async def test_create_application_job_title_not_valid(
+    client: AsyncClient,
+    dbsession: AsyncSession,
+    fastapi_app: FastAPI,
+    application_request_body: ApplicationCreateMessage,
+) -> None:
+
+    application_request_body.job_title = ""
+    request_body = application_request_body.dict()
+    url = fastapi_app.url_path_for("create_application")
+    headers = get_user_token_headers()
+    response = await client.post(url=url, headers=headers, json=request_body)
+    response_data = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response_data["detail"] == [
+        {
+            "loc": ["body", "job_title"],
+            "msg": "job_title cannot be less 1 characters",
+            "type": "value_error",
+        },
+    ]
+
+
+@pytest.mark.anyio
+async def test_create_application_status_not_valid(
+    client: AsyncClient,
+    dbsession: AsyncSession,
+    fastapi_app: FastAPI,
+    application_request_body: ApplicationCreateMessage,
+) -> None:
+
+    application_request_body.status = ""
+    request_body = application_request_body.dict()
+    url = fastapi_app.url_path_for("create_application")
+    headers = get_user_token_headers()
+    response = await client.post(url=url, headers=headers, json=request_body)
+    response_data = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response_data["detail"] == [
+        {
+            "loc": ["body", "status"],
+            "msg": "status cannot be less 1 characters",
+            "type": "value_error",
+        },
+    ]
+
+
+@pytest.mark.anyio
+async def test_create_application_attractiveness_scale_not_valid(  # noqa:WPS118
+    client: AsyncClient,
+    dbsession: AsyncSession,
+    fastapi_app: FastAPI,
+    application_request_body: ApplicationCreateMessage,
+) -> None:
+
+    application_request_body.attractiveness_scale = 100
+    request_body = application_request_body.dict()
+    url = fastapi_app.url_path_for("create_application")
+    headers = get_user_token_headers()
+    response = await client.post(url=url, headers=headers, json=request_body)
+    response_data = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response_data["detail"] == [
+        {
+            "loc": ["body", "attractiveness_scale"],
+            "msg": "attractiveness_scale must be in range 1..5",
+            "type": "value_error",
+        },
+    ]
+
+
+@pytest.mark.anyio
+async def test_create_application_status_category_not_valid(  # noqa:WPS118
+    client: AsyncClient,
+    dbsession: AsyncSession,
+    fastapi_app: FastAPI,
+    application_request_body: ApplicationCreateMessage,
+) -> None:
+
+    application_request_body.status_category = "hello"  # type: ignore
+    request_body = application_request_body.dict()
+    url = fastapi_app.url_path_for("create_application")
+    headers = get_user_token_headers()
+    response = await client.post(url=url, headers=headers, json=request_body)
+    response_data = json.loads(response.content)
+
+    assert response.status_code == status.HTTP_422_UNPROCESSABLE_ENTITY
+    assert response_data["detail"][0]["type"] == "type_error.enum"
